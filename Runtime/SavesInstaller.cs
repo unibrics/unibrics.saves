9ae8e03975589319c193f070315f1b96a6f796af
artem.korotenko <artem.korotenko@betterme.world>
2021-04-06 23:37:36 +0300

    added links and gitignore

---
+++ Runtime/SavesInstaller.cs
@@ -0,0 +1,14 @@
+﻿namespace Unibrics.Saves
+{
+    using Core;
+    using Core.Services;
+
+    [Install]
+    public class SavesInstaller : ModuleInstaller
+    {
+        public override void Install(IServicesRegistry services)
+        {
+            
+        }
+    }
+}
